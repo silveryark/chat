60a1598bb9379c6d25447cdbf6320dc5d8c4b0c2
Jingsi <lookisliu@gmail.com>
2018-09-12 17:46:34 +0800

    move config to server

--- src/main/java/com/silveryark/chat/ChatApplication.java
+++ src/main/java/com/silveryark/chat/ChatApplication.java
@@ -1,6 +1,7 @@
 package com.silveryark.chat;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.web.reactive.config.EnableWebFlux;
